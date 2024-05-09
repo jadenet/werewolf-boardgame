843c1344c3cc56dfcd9970430105a6d38f51071b
Jaden <97551221+jadenet@users.noreply.github.com>
2024-05-09 13:54:48 -0400

    Improve create lobby page UI

--- frontend/src/routes/createlobby.tsx
+++ frontend/src/routes/createlobby.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import roles from "../../../backend/assets/roles.json";
 
 const chats = ["Video", "Audio", "Text"];
 const languages = ["English", "Dutch", "Spanish"];
+const visibilityTypes = ["Public", "Invite Only"];
 
 let gamemodes = [
   {
@@ -18,7 +19,7 @@ let gamemodes = [
     name: "Custom",
     role_percentages: { werewolves: 5, solos: 5, villagers: 90 },
     roles: [],
-  }
+  },
 ];
 
 const villageRoles = roles
@@ -45,11 +46,11 @@ const soloRoles = roles
     return a.name > b.name ? 1 : -1;
   });
 
-
-
 export default function CreateLobby() {
   const [currentGamemode, setCurrentGamemode]: any = useState(gamemodes[0]);
   const [currentRoles, setCurrentRoles]: any = useState(gamemodes[0].roles);
+  const customGamemode = useMemo(() => currentRoles, [currentRoles]);
+  gamemodes[1].roles = customGamemode;
 
   function checkCurrentGamemode(e: any) {
     let newRoles = [...currentRoles];
@@ -74,106 +75,45 @@ export default function CreateLobby() {
         method="POST"
         className="flex flex-col gap-8 items-center py-4 mb-20 w-full"
       >
-        <div className="max-h-xl outline outline-2 outline-base-300 flex gap-4 justify-around items-center p-8 w-full max-w-7xl h-full rounded-lg">
-          <div className="flex flex-col gap-8 w-1/3 px-10">
-            <div className="flex flex-col flex-1 gap-4 items-center">
-              <div className="w-full text-lg text-center">Gamemode</div>
-              <div className="flex flex-wrap gap-3 justify-center">
-                {gamemodes.map((gamemode, index) => {
-                  function handleGamemodeChange() {
-                    setCurrentGamemode(gamemode);
-                  }
-
-                  return (
-                    <input
-                      type="radio"
-                      aria-label={gamemode.name}
-                      name="gamemode"
-                      value={gamemode.name}
-                      className="btn btn-outline"
-                      key={index}
-                      checked={currentGamemode.name === gamemode.name}
-                      onChange={handleGamemodeChange}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-            <div className="flex flex-col flex-1 gap-4 items-center">
-              <div className="w-full text-lg text-center">Chat</div>
-              <div className="flex flex-wrap gap-3 justify-center w-full">
-                {chats.map((chat, index) => {
-                  return (
-                    <input
-                      type="checkbox"
-                      aria-label={chat}
-                      name="chat"
-                      className="btn btn-outline"
-                      key={index}
-                      value={chat}
-                      defaultChecked={chat === "Audio" || chat === "Text"}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-
-            <div className="flex flex-col flex-1 gap-4 items-center">
-              <div className="w-full text-lg text-center">Languages</div>
-              <div className="flex flex-wrap gap-3 justify-center">
-                {languages.map((language, index) => {
-                  return (
-                    <input
-                      type="checkbox"
-                      aria-label={language}
-                      name="language"
-                      className="btn btn-outline"
-                      key={index}
-                      value={language}
-                      defaultChecked={language === "English"}
-                    />
-                  );
-                })}
-              </div>
-            </div>
-
-            <button type="submit" className="btn btn-primary w-full">
-              Submit
-            </button>
-          </div>
-
-          <div className="flex flex-col gap-4 items-center">
+        <div className="max-h-xl flex justify-center gap-4 p-8 w-full max-w-7xl min-h-screen rounded-lg">
+          <div className="flex flex-col gap-4 items-center max-w-3xl">
             <div className="collapse collapse-arrow bg-base-200">
               <input type="radio" name="roles" defaultChecked />
               <div className="collapse-title text-xl font-medium">
                 Village Roles
               </div>
               <div className="collapse-content">
-                <div className="grid grid-cols-4 gap-4 items-center p-4">
+                <div className="grid auto-rows-fr grid-cols-3 gap-4 justify-center items-center p-4">
                   {villageRoles.map((role) => {
+                    const isChecked = currentGamemode.roles.includes(role.name);
                     return (
-                      <>
+                      <div
+                        className={`flex h-full justify-between items-center outline-secondary rounded-lg${
+                          !isChecked ? " outline outline-1 outline-secondary opacity-15" : " bg-secondary bg-opacity-15"
+                        }`}
+                      >
                         <input
                           type="checkbox"
                           name="role"
                           id={role.name}
                           value={role.name}
-                          defaultChecked={currentGamemode.roles.includes(role.name)}
+                          defaultChecked={isChecked}
                           onChange={checkCurrentGamemode}
+                          className="accent-secondary hidden"
                         />
                         <label
                           htmlFor={role.name}
-                          className="tooltip flex gap-4 items-center"
+                          className="tooltip flex gap-4 items-center w-full p-2 px-4"
                           data-tip={role.name}
                         >
                           <img
                             src={`/images/roles/${role.img}`}
                             alt={role.name}
-                            className="aspect-square object-contain w-10"
+                            className="aspect-square object-contain w-8"
                           />
-                          {role.name}
+                          <p className="text-start">{role.name}</p>
                         </label>
-                      </>
+                      </div>
                     );
                   })}
                 </div>
@@ -185,33 +125,37 @@ export default function CreateLobby() {
                 Werewolf Roles
               </div>
               <div className="collapse-content">
-                <div className="grid grid-cols-4 gap-4 items-center p-4">
+                <div className="grid auto-rows-fr grid-cols-3 gap-4 items-center p-4">
                   {werewolfRoles.map((role) => {
+                    const isChecked = currentGamemode.roles.includes(role.name);
                     return (
-                      <>
+                      <div
+                      className={`flex h-full justify-between items-center outline-secondary rounded-lg${
+                        !isChecked ? " outline outline-1 outline-secondary opacity-15" : " bg-secondary bg-opacity-15"
+                      }`}
+                      >
                         <input
                           type="checkbox"
                           name="role"
                           id={role.name}
                           value={role.name}
-                          defaultChecked={currentGamemode.roles.includes(
-                            role.name
-                          )}
+                          defaultChecked={isChecked}
                           onChange={checkCurrentGamemode}
+                          className="accent-secondary hidden"
                         />
                         <label
                           htmlFor={role.name}
-                          className="tooltip flex gap-4 items-center"
+                          className="tooltip flex gap-4 items-center w-full p-2 px-4"
                           data-tip={role.name}
                         >
                           <img
                             src={`/images/roles/${role.img}`}
                             alt={role.name}
-                            className="aspect-square object-contain w-10"
+                            className="aspect-square object-contain w-8"
                           />
-                          {role.name}
+                          <p className="text-start">{role.name}</p>
                         </label>
-                      </>
+                      </div>
                     );
                   })}
                 </div>
@@ -223,38 +167,128 @@ export default function CreateLobby() {
                 Solo Roles
               </div>
               <div className="collapse-content">
-                <div className="grid grid-cols-4 gap-4 items-center p-4">
+                <div className="grid auto-rows-fr grid-cols-3 gap-4 items-center p-4">
                   {soloRoles.map((role) => {
+                    const isChecked = currentGamemode.roles.includes(role.name);
                     return (
-                      <>
+                      <div
+                      className={`flex h-full justify-between items-center outline-secondary rounded-lg${
+                        !isChecked ? " outline outline-1 outline-secondary opacity-15" : " bg-secondary bg-opacity-15"
+                      }`}
+                      >
                         <input
                           type="checkbox"
                           name="role"
                           id={role.name}
                           value={role.name}
-                          defaultChecked={currentGamemode.roles.includes(
-                            role.name
-                          )}
+                          defaultChecked={isChecked}
                           onChange={checkCurrentGamemode}
+                          className="accent-secondary hidden"
                         />
                         <label
                           htmlFor={role.name}
-                          className="tooltip flex gap-4 items-center"
+                          className="tooltip flex gap-4 items-center w-full p-2 px-4"
                           data-tip={role.name}
                         >
                           <img
                             src={`/images/roles/${role.img}`}
                             alt={role.name}
-                            className="aspect-square object-contain w-10"
+                            className="aspect-square object-contain w-8"
                           />
-                          {role.name}
+                          <p className="text-start">{role.name}</p>
                         </label>
-                      </>
+                      </div>
                     );
                   })}
                 </div>
               </div>
             </div>
+          </div>
+
+          <div className="flex flex-col gap-8 w-1/3 px-10">
+            <div className="flex flex-col gap-4">
+              <div className="w-full text-lg">Gamemode</div>
+              <div className="flex flex-wrap gap-3">
+                {gamemodes.map((gamemode, index) => {
+                  function handleGamemodeChange() {
+                    setCurrentGamemode(gamemode);
+                  }
+
+                  return (
+                    <input
+                      type="radio"
+                      aria-label={gamemode.name}
+                      name="gamemode"
+                      value={gamemode.name}
+                      className="btn btn-outline btn-sm"
+                      key={index}
+                      checked={currentGamemode.name === gamemode.name}
+                      onChange={handleGamemodeChange}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+            <div className="flex flex-col gap-4">
+              <div className="w-full text-lg">Chat</div>
+              <div className="flex flex-wrap gap-3 w-full">
+                {chats.map((chat, index) => {
+                  return (
+                    <input
+                      type="checkbox"
+                      aria-label={chat}
+                      name="chat"
+                      className="btn btn-outline btn-sm"
+                      key={index}
+                      value={chat}
+                      defaultChecked={chat === "Audio" || chat === "Text"}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div className="w-full text-lg">Languages</div>
+              <div className="flex flex-wrap gap-3">
+                {languages.map((language, index) => {
+                  return (
+                    <input
+                      type="checkbox"
+                      aria-label={language}
+                      name="language"
+                      className="btn btn-outline btn-sm"
+                      key={index}
+                      value={language}
+                      defaultChecked={language === "English"}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div className="w-full text-lg">Visibility</div>
+              <div className="flex flex-wrap gap-3">
+                {visibilityTypes.map((visibility, index) => {
+                  return (
+                    <input
+                      type="radio"
+                      aria-label={visibility}
+                      name="visibility"
+                      className="btn btn-outline btn-sm"
+                      key={index}
+                      value={visibility}
+                      defaultChecked={visibility === "Public"}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+
+            <button type="submit" className="btn btn-primary w-full">
+              Submit
+            </button>
           </div>
         </div>
       </form>
