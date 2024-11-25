efb20fae64e1f53d547dab282e5381fc56c8aff6
jadenet <jaedwards35@proton.me>
2024-11-25 09:11:11 -0500

    Improve leave button

--- frontend/src/components/LeaveModal.tsx
+++ frontend/src/components/LeaveModal.tsx
@@ -1,4 +1,12 @@
+import { useLocation } from "wouter";
+
 export default function LeaveModal(props) {
+  const [, setLocation] = useLocation();
+
+  function leaveLobby() {
+    setLocation("/");
+  }
+
   return (
     <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
       <div className="modal-box">
@@ -8,13 +16,20 @@ export default function LeaveModal(props) {
           the game?
           {props.currentPlayer &&
             props.currentPlayer.isHost &&
-            " This will kick all of the players and delete the lobby."}
+            " This will kick all of the players and remove the lobby."}
         </p>
         <div className="modal-action flex flex-2">
           <form method="dialog">
             <button className="btn w-16">No</button>
           </form>
-          <button className="btn btn-error w-16">Yes</button>
+          <button
+            className="btn btn-error w-16"
+            onClick={() => {
+              leaveLobby();
+            }}
+          >
+            Yes
+          </button>
         </div>
       </div>
       <form method="dialog" className="modal-backdrop">
